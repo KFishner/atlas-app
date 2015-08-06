4d70b8bef326590bc4fac5d1f30e99bd9a1eaae3
Kevin Fishner <KFishner@gmail.com>
2015-08-06 10:24:17 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Welcome to Atlas!");
+  response.write("Demo Complete!");
   response.end();
 }).listen(8888);
