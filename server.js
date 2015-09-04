2f86fa5930ccf97331f98b76ee9069dba1d42fec
Kevin Fishner <KFishner@gmail.com>
2015-09-04 07:35:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Google!");
+  response.write("Hello Barclays!");
   response.end();
 }).listen(8888);
