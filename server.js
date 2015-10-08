75838207c09b7030cb494efaad453b32dc8535aa
Kevin Fishner <KFishner@gmail.com>
2015-10-08 12:34:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Cap1!");
+  response.write("Hello WP!");
   response.end();
 }).listen(8888);
